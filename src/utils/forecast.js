91b068483a2a4bd0463ab154ef5c30d14996ef68
Vedant Jain <vedant.jain.indore@gmail.com>
2022-01-12 21:15:47 +0530

    Add to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5640e63337a0b66b5fec216957bb0093&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently"+ response.body.current.temperature + " degress out. The high today is  "+ body.daily.data[0].temperatureHigh +" with a low of "+ body.daily.data[0].temperatureLow + ". " )
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
