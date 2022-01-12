77cc91ee2daf81fd79eae6ab6644aef6d1402fca
Vedant Jain <vedant.jain.indore@gmail.com>
2022-01-12 08:04:33 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5640e63337a0b66b5fec216957bb0093&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
